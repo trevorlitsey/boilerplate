0aa8d2a1505236b7d3e052e65e1e85f3711355ea
Trevor Litsey <trevorlitsey@gmail.com>
2019-03-31 16:22:24 -0500

    oops re-base

--- src/firebase.js
+++ src/firebase.js
@@ -1,42 +1,51 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
-import Rebase from 're-base';
 
 const app = firebase.initializeApp({
-	apiKey: 'AIzaSyA2xwZ2FGGJXcTOku1RUvu-xq57bKADXiQ',
-	authDomain: 'boilerplate-tl.firebaseapp.com',
-	databaseURL: 'https://boilerplate-tl.firebaseio.com',
-	projectId: 'boilerplate-tl',
-	storageBucket: 'boilerplate-tl.appspot.com',
-	messagingSenderId: '517479425728'
+  apiKey: 'AIzaSyA2xwZ2FGGJXcTOku1RUvu-xq57bKADXiQ',
+  authDomain: 'boilerplate-tl.firebaseapp.com',
+  databaseURL: 'https://boilerplate-tl.firebaseio.com',
+  projectId: 'boilerplate-tl',
+  storageBucket: 'boilerplate-tl.appspot.com',
+  messagingSenderId: '517479425728',
 });
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-
 // -------- exports ---------
 
 export const signIn = () => {
-	firebase.auth().signInWithPopup(provider).then((result) => {
-		// hooray
-	}).catch((error) => {
-		return console.error(error);
-	});
-}
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      // hooray
+    })
+    .catch(error => {
+      return console.error(error);
+    });
+};
 
 export const signInWithEmailAndPassword = (email, password) => {
-	firebase.auth().signInWithEmailAndPassword(email, password).catch((err) => {
-		return console.error(err)
-	});
-}
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(err => {
+      return console.error(err);
+    });
+};
 
 export const signOut = () => {
-	firebase.auth().signOut().then(() => {
-		// Sign-out successful.
-	}).catch((error) => {
-		console.error(error);
-	});
-}
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
 
 export const db = app.firestore();
 
