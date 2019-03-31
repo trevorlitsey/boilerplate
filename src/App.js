ce9cd3821d495d3fac4400156a1563c37a060965
Trevor Litsey <trevorlitsey@gmail.com>
2019-03-31 16:56:07 -0500

    bootsrap via cdn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import firebase, { db } from './firebase';
 
@@ -10,13 +10,13 @@ import NotFound from './pages/NotFound';
 import './styles/styles.scss';
 
 const App = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={Index} />
-			<Route exact path="/snippets" component={Snippets} />
-			<Route component={NotFound} />
-		</Switch>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/snippets" component={Snippets} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const P = styled.p`
-	text-align: center;
-	color: grey;
-`
+  text-align: center;
+  color: grey;
+`;
 
 const Footer = () => (
-	<P> <a href="http://www.trevorlitsey.com" target="blank">trevorlitsey.com</a> | <a href="https://github.com/trevorlitsey/cashflow-calc" target="blank">github</a> </P>
-)
+  <P>
+    {' '}
+    <a href="http://www.trevorlitsey.com" target="blank">
+      trevorlitsey.com
+    </a>{' '}
+    |{' '}
+    <a href="https://github.com/trevorlitsey/boilerplate" target="blank">
+      github
+    </a>{' '}
+  </P>
+);
 
 // no props
 
